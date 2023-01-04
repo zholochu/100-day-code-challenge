b5c7b1a350c2addaaac32d6a93be5a6d63d9dd09
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-01-04 11:26:13 -0500

    day-9 challenge done with the help from solutions part

--- day-9-regular-expression-matching.rb
+++ day-9-regular-expression-matching.rb
@@ -3,14 +3,11 @@
 # '*' Matches zero or more of the preceding element.
 # The matching should cover the entire input string (not partial).
 
-def is_match(s, p)
-  s_array = s.chars
-  p_array = p.chars
-  p_index = 0
-  s_index = 0
-  if s_array == p_array || p_array.first == "*"
-    return true
-  elsif s_array != p_array
-    return false
+def is_match s, p
+  if p[/[\.\*]/]
+      p = Regexp.new p
+      (!!s[p]) && (s[p].size == s.size)
+  else
+      s == p
   end
 end
