0f24277f4270fb57b58b05dc9e15b6500951e03c
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-01-09 11:46:41 -0500

    Code update

--- day-14-longest-common-prefix.rb
+++ day-14-longest-common-prefix.rb
@@ -7,11 +7,14 @@
 def longest_common_prefix(strs)
   return "" if strs.empty?
   prefix = strs[0]
-  strs.each do |str|
-    while str[0, prefix.length] != prefix
+  strs.each do |element|
+    while element[0, prefix.length] != prefix
       prefix = prefix[0, prefix.length-1]
       break if prefix.empty?
     end
   end
   prefix
 end
+
+strs = ["flower","flow","flight"]
+strs = ["dog","racecar","car"]
