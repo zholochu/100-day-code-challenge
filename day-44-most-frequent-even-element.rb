3832aa28e702dd1dbd211de4c34b9c0c365ebb58
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-02-10 10:54:08 -0500

    Day 44 first challenge solved

---
+++ day-44-most-frequent-even-element.rb
@@ -0,0 +1,11 @@
+# Given an integer array nums, return the most frequent even element.
+# If there is a tie, return the smallest one. If there is no such element, return -1.
+# @param {Integer[]} nums
+# @return {Integer}
+def most_frequent_even(nums)
+  nums = nums.sort
+  nums.each do |digit|
+  end
+end
+
+pp most_frequent_even([0,1,2,2,4,4,1])
