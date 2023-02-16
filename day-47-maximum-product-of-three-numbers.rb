b60ebb2857b1c4caba916b4caecf943d4adedaf8
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-02-15 23:23:11 -0500

    Day 47 first challenge done

--- day-47-maximum-product-of-three-numbers.rb
+++ day-47-maximum-product-of-three-numbers.rb
@@ -3,14 +3,11 @@
 # @return {Integer}
 def maximum_product(nums)
   nums = nums.sort
-  negative_counter = 0
-  nums.each do |element|
-    negative_counter += 1 if element.negative?
-  end
-  if negative_counter >= 2
-
-  end
-  negative_counter
+  first_three = nums[-1] * nums[-2] * nums[-3]
+  second_three = nums[-1] * nums[1] * nums [2]
+  first_three > second_three ? first_three : second_three
 end
 
 pp maximum_product([-1, 5, -10, -2, -3, 4, 5])
+pp maximum_product([-4,-3,-2,-1,60])
+pp maximum_product([-5,-6,-5])
