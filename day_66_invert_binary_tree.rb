062116cb55a925fa667b9b8ead49496c1782c0bb
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-03-09 23:21:28 -0500

    Day 66 first challenge done

---
+++ day_66_invert_binary_tree.rb
@@ -0,0 +1,35 @@
+# Given the root of a binary tree, invert the tree, and return its root.
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+  result = [@val]
+  if root.empty?
+    root
+  end
+end
+
+pp invert_tree([4,2,7,1,3,6,9])
+pp invert_tree([2,1,3])
+pp invert_tree([])
+
+
+# Example 1:
+# Input: root = [4,2,7,1,3,6,9]
+# Output: [4,7,2,9,6,3,1]
+
+# Example 2:
+# Input: root = [2,1,3]
+# Output: [2,3,1]
+
+# Example 3:
+# Input: root = []
+# Output: []
