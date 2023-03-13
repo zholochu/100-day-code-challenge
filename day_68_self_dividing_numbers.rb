715b6e9138ba5ef45560bb5d93c5f2e263fd14bb
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-03-13 00:34:30 -0400

    Day 68 second challenge done

---
+++ day_68_self_dividing_numbers.rb
@@ -0,0 +1,47 @@
+# A self-dividing number is a number that is divisible by every digit it contains.
+# For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+# A self-dividing number is not allowed to contain the digit zero.
+# Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right].
+# @param {Integer} left
+# @param {Integer} right
+# @return {Integer[]}
+def self_dividing_numbers(left, right)
+  array = (left..right).to_a
+  final_array = []
+  array.each { |num| final_array << num if !num.to_s.include?("0")}
+  array_ind = 0
+  array.length.times do
+    final_array << array[array_ind] if !array[array_ind].to_s.include?("0")
+    array_ind += 1
+  end
+
+  length = final_array.length
+  result = []
+  index = 0
+  length.times do
+    interim_array = final_array[index].to_s.chars.map { |chr| chr = chr.to_i }
+    counter = 0
+    interim_array.each do |element|
+      if final_array[index] % element != 0
+        counter += 1
+      end
+    end
+    result << final_array[index] if counter == 0
+    index += 1
+  end
+  result
+end
+
+pp self_dividing_numbers(1, 22)
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22]
+pp self_dividing_numbers(47, 85)
+pp self_dividing_numbers(100, 141)
+
+
+# Example 1:
+# Input: left = 1, right = 22
+# Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+
+# Example 2:
+# Input: left = 47, right = 85
+# Output: [48,55,66,77]
