73c851a0e68de44d36d77f187b37a25fc9380532
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-03-17 21:15:14 -0400

    Day 71 second challenge done

---
+++ day_71_count_items_matching_a_rule.rb
@@ -0,0 +1,38 @@
+# You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+# The ith item is said to match the rule if one of the following is true:
+# ruleKey == "type" and ruleValue == typei.
+# ruleKey == "color" and ruleValue == colori.
+# ruleKey == "name" and ruleValue == namei.
+# Return the number of items that match the given rule.
+# @param {String[][]} items
+# @param {String} rule_key
+# @param {String} rule_value
+# @return {Integer}
+def count_matches(items, rule_key, rule_value)
+  rule_key_index = 0
+  case rule_key
+  when "type"
+    rule_key_index = 0
+  when "color"
+    rule_key_index = 1
+  when "name"
+    rule_key_index = 2
+  end
+  counter = 0
+  items.each do |element|
+    if element[rule_key_index] == rule_value
+      counter += 1
+    end
+  end
+  counter
+end
+
+# Example 1:
+# Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+# Output: 1
+# Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+
+# Example 2:
+# Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+# Output: 2
+# Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
