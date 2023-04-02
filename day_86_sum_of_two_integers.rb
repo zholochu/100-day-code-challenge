623f05ed4fd1097fc02fe54c668551827d139fbb
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-04-02 16:08:16 -0400

    Day 86 third challenge done

---
+++ day_86_sum_of_two_integers.rb
@@ -0,0 +1,15 @@
+# Given two integers a and b, return the sum of the two integers without using the operators + and -.
+# @param {Integer} a
+# @param {Integer} b
+# @return {Integer}
+def get_sum(a, b)
+  array = [a, b].sum
+end
+
+# Example 1:
+# Input: a = 1, b = 2
+# Output: 3
+
+# Example 2:
+# Input: a = 2, b = 3
+# Output: 5
