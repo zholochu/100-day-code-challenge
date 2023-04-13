6d0580d43737992882d9523fc490b13093c939c0
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-04-12 22:26:30 -0400

    Day 92 second challenge done

--- day_92_3sum_closest.rb
+++ day_92_3sum_closest.rb
@@ -1,6 +1,36 @@
 # Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
 # Return the sum of the three integers.
 # You may assume that each input would have exactly one solution.
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def three_sum_closest(nums, target)
+  nums.sort!
+  closest_sum = nums[0] + nums[1] + nums[2]
+
+  for i in 0..(n-2)
+    left = i + 1
+    right = n - 1
+
+    while left < right
+      sum = nums[i] + nums[left] + nums[right]
+
+      if (sum - target).abs < (closest_sum - target).abs
+        closest_sum = sum
+      end
+
+      if sum > target
+        right -= 1
+      elsif sum < target
+        left += 1
+      else
+        return sum
+      end
+    end
+  end
+
+  return closest_sum
+end
 
 
 
