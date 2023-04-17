f20713335e5ea667e34a7378a5ca86528d6108d7
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-04-17 16:49:25 -0400

    Day 97 second challenge done

--- day_97_queue_reconstruction_by_height.rb
+++ day_97_queue_reconstruction_by_height.rb
@@ -1,5 +1,16 @@
 # You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
 # Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+def reconstruct_queue(people)
+  people.sort! { |a, b| a[0] == b[0] ? a[1] - b[1] : b[0] - a[0] }
+
+  queue = []
+
+  people.each do |p|
+    queue.insert(p[1], p)
+  end
+
+  queue
+end
 
 
 # Example 1:
